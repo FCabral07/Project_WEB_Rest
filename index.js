bd810d948fe3812d4f95c5e182d574422152b38b
Arthur Figueirêdo Virginio Penha <102666688+arthurltsdev@users.noreply.github.com>
2023-09-03 16:46:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-'use strict'
+"use strict";
 
 // Importações
-const mongoose = require('mongoose');
-const express = require('express');
-const userRouter = require('./src/routers/userRouter');
-const productRouter = require('./src/routers/productRouter');
-// const authRouter = require('./routers/authRouter');
+const mongoose = require("mongoose");
+const express = require("express");
+const userRouter = require("./src/routers/userRouter");
+const productRouter = require("./src/routers/productRouter");
+const pedidoRouter = require("./src/routers/pedidoRoute");
+const promocaoRouter = require("./src/routers/promocaoRoute");
 
 // Conectando o mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/project');
+mongoose.connect(
+  "mongodb+srv://arthurpenha:devhouse@devhouse.jdojpuo.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Criando o app e definindo para usar os routers
 const app = express();
 app.use(express.json());
 app.use(userRouter);
+app.use(pedidoRouter);
 app.use(productRouter);
+app.use(promocaoRouter);
 // app.use(authRouter);
 
 // Definindo a porta
-const port = process.env.PORT || 3000;
-  
+const port = 3333;
+
 // Iniciar o servidor
 app.listen(port, () => {
-    console.log(`O servidor está rodando na porta ${port}`);
+  console.log(`O servidor está rodando na porta ${port}`);
 });
