740186e96b9750f49c79cc001065f4a35f259ab2
Arthur Figueirêdo Virginio Penha <102666688+arthurltsdev@users.noreply.github.com>
2023-10-03 16:06:57 -0300

    Update promocaoRouter.js
    Atualização do promocaoRouter para Entrega Fase 2

--- src/routers/promocaoRoute.js
+++ src/routers/promocaoRoute.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const router = express.Router();
 const promocaoController = require("../controllers/promocaoController");
+const authMiddleware = require("../authMiddleware");
+const roleMiddleware = require("../roleMiddleware");
 
-const promocaoRouter = express.Router();
+//A rota GET de todas as promoções está em productRouter, logo, todas as demais rotas de promocaoRoute são autenticadas.
+router.use("/produtos", authMiddleware, roleMiddleware);
 
-// Rota para aplicar desconto
-promocaoRouter
-  .route("/aplicarDesconto")
-  .post((req, res) => promocaoController.aplicarDesconto(req, res));
 
-// Rota para obter produtos com desconto
-promocaoRouter
-  .route("/getProdutosComDesconto")
-  .get((req, res) => promocaoController.getProdutosComDesconto(req, res));
 
-module.exports = promocaoRouter;
+// Rota para adicionar uma nova promoção a um produto
+router.post(
+  "/produtos/:codigoProduto/promocoes",
+  promocaoController.adicionarPromocao
+);
+
+// Rota para excluir uma promoção
+router.delete(
+  "/produtos/:codigoProduto/promocoes",
+  promocaoController.removerPromocao
+);
+
+// Rota para alterar a porcentagem de uma promoção
+router.put(
+  "/produtos/:codigoProduto/promocoes",
+  promocaoController.atualizarPromocao
+);
+
+module.exports = router;
